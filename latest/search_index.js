103f805c083139f52962e330d01fb6af48622908
autodocs <autodocs>
2018-05-27 22:19:21 +0000

    build based on 303b171

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "MPI Reco",
     "category": "section",
-    "text": "Initiative for open magnetic particle imaging data"
+    "text": "Julia package for reconstruction of magnetic particle imaging (MPI) data"
 },
 
 {
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Introduction",
     "category": "section",
-    "text": "Magnetic particle imaging (MPI) is a tomographic imaging technique that allows to determine the spatial distribution of magnetic nanoparticles (MNPs). The aim of this project is to provide MPI data that are freely available for the research community. To facilitate reproducible research all available information including a detailed description of the used phantoms and the applied measurement sequences are provided.All MPI data is stored in the MPI Data Format (MDF). The MDF provides a common data format for the storage of MPI raw data, calibration data, and reconstruction data.This projects comes with a first set of datasets covering the most interesting cases to get started with MPI. New datasets can be easily added and researcher worldwide are invited to contribute own datasets."
+    "text": "This project provides functions for the reconstruction of MPI data. The project is implemented in the programming language Julia and contains algorithms forregular 1D / 2D / 3D image reconstruction using a system matrix based approach\nmulti-patch and multi-gradient reconstruction for data the has been acquired using a focus field sequence\nmulti-colored image reconstruction"
 },
 
 {
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "License / Terms of Usage",
     "category": "section",
-    "text": "The source code of this project is licensed under the MIT license. This implies that you are free to use, share, and adapt the data. However, you must give appropriate credit by citing the project."
+    "text": "The source code of this project is licensed under the MIT license. This implies that you are free to use, share, and adapt it. However, you must give appropriate credit by citing the project."
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Contact",
     "category": "section",
-    "text": "If you have problems using the datasets, find mistakes, or have general questions please use the issue tracker to contact us. You can also contact Tobias Knopp directly."
+    "text": "If you have problems using the software, find mistakes, or have general questions please use the issue tracker to contact us."
 },
 
 {
