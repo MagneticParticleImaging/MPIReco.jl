e82b691c05fdc2f7b0db4df68cff4168c4612981
Tobias Knopp <tobias@knoppweb.de>
2018-05-30 16:46:56 +0200

    cleanup

--- test/MultiPatch.jl
+++ test/MultiPatch.jl
@@ -11,14 +11,14 @@ b = MultiMPIFile(["dataMP01", "dataMP02", "dataMP03", "dataMP04"])
 figure(7)
 
 @time c1 = reconstruction(bSF, b;
-                   SNRThresh=5, frames=1, minFreq=80e3, OffsetSF=0,OffsetFF=[0.008,0.008,0.0005],
+                   SNRThresh=5, frames=1, minFreq=80e3,
                    recChannels=1:2,iterations=1, spectralLeakageCorrection=false)
 
 subplot(2,2,1)
 imshow(c1[:,:,1,1])
 
 @time c2 = reconstruction(bSF, b;
-                   SNRThresh=5, frames=1, minFreq=80e3, OffsetSF=0,OffsetFF=[0.008,0.008,0.0005],
+                   SNRThresh=5, frames=1, minFreq=80e3,
                    recChannels=1:2,iterations=1, roundPatches=true, spectralLeakageCorrection=false)
 
 subplot(2,2,2)
@@ -30,7 +30,7 @@ imshow(c2[:,:,1,1])
 bSFs = MultiMPIFile(["SF_MP01", "SF_MP02", "SF_MP03", "SF_MP04"])
 
 @time c3 = reconstruction(bSFs, b;
-                   SNRThresh=5, frames=1, minFreq=80e3, OffsetSF=0,OffsetFF=[0.0001,0.0001,0.0005],
+                   SNRThresh=5, frames=1, minFreq=80e3,
                    recChannels=1:2,iterations=1, spectralLeakageCorrection=false)
 
 subplot(2,2,3)
@@ -38,7 +38,7 @@ imshow(c3[:,:,1,1])
 
 
 
-# MultiPatch MultiSF
+# Flexible MultiPatch construction
 bSFs = MultiMPIFile(["SF_MP01", "SF_MP02", "SF_MP03", "SF_MP04"])
 mapping = [1,2,3,4]
 freq = filterFrequencies(bSFs, SNRThresh=5, minFreq=80e3)
@@ -51,7 +51,7 @@ FFPos[:,3] = [0.008, 0.008, 0.0]
 FFPos[:,4] = [0.008, -0.008, 0.0]
 
 @time c4 = reconstruction(bSFs, b;
-                   SNRThresh=5, frames=1, minFreq=80e3, OffsetSF=0,OffsetFF=[0.0001,0.0001,0.0005],
+                   SNRThresh=5, frames=1, minFreq=80e3,
                    recChannels=1:2,iterations=1, spectralLeakageCorrection=false, mapping=mapping,
                    systemMatrices = S, SFGridCenter=SFGridCenter, FFPos=FFPos, FFPosSF=FFPos)
 
