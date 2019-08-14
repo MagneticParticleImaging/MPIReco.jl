44c903d2db55e3dd806f8eeb7b52a46e97519518
Tobias Knopp <tobias@knoppweb.de>
2019-08-14 21:41:19 +0000

    add motion compensation test

--- test/MotionCompensation.jl
+++ test/MotionCompensation.jl
@@ -2,9 +2,50 @@ using MPIReco
 
 @testset "motion compensation reconstruction" begin
 
-  bSF = MultiMPIFile(["motionComp/SF1Small.mdf", "motionComp/SF2Small.mdf",
-                      "motionComp/SF3Small.mdf", "motionComp/SF4Small.mdf"])
-  bBG = MPIFile("motionComp/measBG.mdf")
-  b = MPIFile("motionComp/measFast.mdf") # "measSlow.mdf"
+  ################
+  ## Parameters ##
+  ################
+  alpha = 0.4 
+
+  # Choose the peak number according to the expected approximate value. Be careful, if there are peaks not corresponding to object motion, division by choosePeak can be false.
+  # For experiments with different periodic motions it is used to pick one motion frequency (then don't divide by choosePeak)
+
+  choosePeak = 4  
+
+  windowType = 1  # 1: Hann, 2: FT1A05, 3: Rectangle
+
+  # Measurement data
+  datadirMeas = "./motionComp/"
+  b = MPIFile(datadirMeas*"measFast.mdf") # high frequency
+  bBG = MPIFile(datadirMeas*"measBG.mdf") # background measurement
+
+  # System matrices
+  datadirSF = "./motionComp/"
+  SFall = ["SF1Small.mdf","SF2Small.mdf","SF3Small.mdf","SF4Small.mdf"]
+  bSF = MultiMPIFile(datadirSF.*SFall)
+
+  # Frequency selection # TODO: reconstructionPeriodicMotion basteln, die das tut
+  freq = filterFrequencies(bSF,SNRThresh=2,minFreq=80e3,recChannels=[1,2,3])
+
+  # Background frames
+  frBG = [1:200,201:400, 401:600 ,601:800]
+
+  # Selected frames
+  recoFrame = 1
+
+  ####################
+  ## Reconstruction ##
+  ####################
+
+  # Reconstruction parameters
+  lambda = 0.01 
+  iterations = 1
+
+  c = reconstructionPeriodicMotion(b, bSF, bBG,
+		frBG, choosePeak, alpha, freq, recoFrame,
+		lambda=lambda, iterations=iterations,
+		windowType=windowType, higherHarmonic=choosePeak)
+
+  exportImage("./img/MotionComp.png", maximum(arraydata(data(c[:,:,:,1])),dims=3)[:,:,1])
 
 end
