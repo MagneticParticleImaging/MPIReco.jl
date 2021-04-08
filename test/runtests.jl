20782e54a4babb46bf0d775cb7463e97a1d26314
Jonas Schumacher <jonas.beuke@gmx.de>
2021-04-08 09:16:31 +0200

    Change artifact to tarball (fixes #12 due to using tarballs instead of zip and #7 by merging PR #8 and enhancing it). Before merging, please exchange the data zip-file with the matching tar.gz.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,29 +2,13 @@ import ImageMagick
 using HTTP
 using Test
 using FileIO
-
 using Scratch
-using Artifacts
+using LazyArtifacts
 
-# TODO: Don't use ZIP, instead upload a tar.gz to somewhere (like Github)
-#       https://github.com/JuliaPackaging/Scratch.jl#can-i-use-a-scratch-space-as-a-temporary-workspace-then-turn-it-into-an-artifact
-
-# To create a new Artifact.toml use https://github.com/simeonschaub/ArtifactUtils.jl
-
-scratch = @get_scratch!("data")
-if isempty(readdir(scratch))
-
-  @info "download data"
-  datapath = joinpath(artifact"data", "data.zip")
-
-  @info "extracting data"
-  cd(scratch) do
-    run(`unzip -oq $datapath`)
-  end
-end
-
-const datadir = joinpath(scratch, "data")
+const datadir = joinpath(artifact"data", "data")
 const imgdir  = @get_scratch!("img")
+
+@info "If you want to check the output of the tests, please head to $imgdir"
 
 function exportImage(filename, I::AbstractMatrix)
   Iabs = abs.(I)
@@ -32,12 +16,12 @@ function exportImage(filename, I::AbstractMatrix)
   save(filename, Icolored )
 end
 
+include("LoadSaveMDF.jl")
+include("Reconstruction.jl")
 include("Cartesian.jl")
 include("MotionCompensation.jl")
-include("Reconstruction.jl")
 include("MultiPatch.jl")
 include("MultiGradient.jl")
 include("Sparse.jl")
 include("SMCenter.jl")
-include("LoadSaveMDF.jl")
 include("SMRecovery.jl")
