b49871c3821f7585a9aedd9adee5da6aec9c0ec4
nHackel <nHackel@users.noreply.github.com>
2023-04-20 13:39:07 +0200

    Add similar function to interface

--- RecoUtils/src/AlgorithmInterface.jl
+++ RecoUtils/src/AlgorithmInterface.jl
@@ -1,4 +1,4 @@
-export AbstractReconstructionAlgorithm, put!, take!, reconstruction
+export AbstractReconstructionAlgorithm, put!, take!, reconstruction, similar
 
 abstract type AbstractReconstructionAlgorithm end
 
@@ -11,3 +11,12 @@ function reconstruction(algo::T, u) where {T<:AbstractReconstructionAlgorithm}
   put!(algo, u)
   return take!(algo)
 end
+
+# process(algoT::Type{T}, ...) as pure helper functions
+# Overwrite process(algo, ...) to mutate struct based on helper function result
+process(algoT::Type{T}, data, param::AbstractReconstructionAlgorithmParameter) where {T<:AbstractReconstructionAlgorithm} = error("No processing defined for algorithm $T with parameter $(typeof(param))")
+process(algo::AbstractReconstructionAlgorithm, data, param::AbstractReconstructionAlgorithmParameter) = process(typeof(algo), data, param)
+
+similar(algo::AbstractReconstructionAlgorithm, data) = error("$(typeof(algo)) must implement similar")
+similar(algo::AbstractReconstructionAlgorithm, data, param::AbstractReconstructionAlgorithmParameter) = similar(typeof(algo), data, param)
+similar(algoT::Type{T}, data, param::AbstractReconstructionAlgorithmParameter) where {T<:AbstractReconstructionAlgorithm} = deepcopy(params)
--- src/SinglePatchAlgorithm.jl
+++ src/SinglePatchAlgorithm.jl
@@ -1,29 +1,37 @@
 # Can be shared across algorithms
 Base.@kwdef struct CommonPreProcessingParameters <: AbstractPreProcessingParameter
-  # ...
-  "Flag whether a background correction shall be applied."
   bgCorrection::Bool = false
-  "Number of frames averages that shall be applied to the raw data. Setting this to `nothing` implies using the default value."
-  numAverages::Union{Integer,Nothing} = nothing
-  "Number of period averages that shall be applied to the raw data. Setting this to `nothing` implies using the default value."
-  numPeriodAverages::Union{Integer,Nothing} = nothing
-  "Number of periods that shall be grouped together in the samples dimension. Setting this to `nothing` implies using the default value."
-  numPeriodGrouping::Union{Integer,Nothing} = nothing
-  # ...
-end
-
-Base.@kwdef struct CommonPostProcessingParameters <: AbstractPostProcessingParameter
-  # ...
+  numAverages::Int64 = 1
+  numPeriodAverages::Int64 = 1
+  numPeriodGrouping::Int64 = 1
+  recChannels::UnitRange{Int64} = 1:rxNumChannels(sf)
 end
 
 # These one can be more tailored to one algo
-Base.@kwdef struct SinglePatchReconstructionParameter <: AbstractReconstructionParameters
-  # ...
-  # SystemMatrix
-  # tempRegularization::Bool = false
-  # solver
-  # lambda
-  # ...
+# Or common system matrix parameter
+Base.@kwdef struct SinglePatchReconstructionParameter{S<:AbstractLinearSolver} <: AbstractReconstructionParameters
+  # File
+  sf::MPIFile
+  loadasreal::Bool = false
+  sparseTrafo::Union{Nothing, Any} = nothing
+  #saveTrafo::Bool = false
+  # Freqs
+  minFreq::Int64 = 0
+  maxFreq::Int64 = rxBandwidth(sf)
+  numUsedFreqs::Int64 = -1
+  # SNR
+  threshSNR::Float64=-1
+  sortBySNR::Bool = false
+  # Solver
+  solver::Type{S}
+  # Grid
+  gridsize::Vector{Int64} = gridSizeCommon(bSF)
+  fov::Vector{Float64} = calibFov(bSF)
+  center::Vector{Float64} = [0.0,0.0,0.0]
+  useDFFoV::Bool = false
+  # Misc.
+  frames::Union{Nothing, UnitRange{Int64}} = nothing
+  deadPixels::Vector{Int64} = Int64[]
 end
 
 # Having the parameters for an algorithm in nested structs allows for easier introspection
@@ -47,9 +55,8 @@ end
 
 function SinglePatchReconstruction(params::SinglePatchReconstructionParameter)
   # Prepare system matrix based on pre and reco params
-  # S, grid = ...
-  # Compute freqs 
-  # freqs = 
+  freqs = getFreqs(params.pre, params.reco)
+  S, grid = getSF(params.pre, params.reco, freqs)
   
   # Also possible depending on the reco params to adapt and change the pre/post params
   # User might input common pre params, but if they want temp. reg., we could build specifc
@@ -65,6 +72,13 @@ function SinglePatchReconstruction(params::SinglePatchReconstructionParameter)
   return SinglePatchReconstruction(params, S, grid, freqs, Channel{Any}(32))
 end
 
+getFreqs(pre::CommonPreProcessingParameters, reco::SinglePatchReconstructionParameter) = filterFrequences(reco.sf, toKargs([pre, reco])...)
+
+# TODO Maybe do this on Type{T}
+function getSF(pre::CommonPreProcessingParameters, reco::SinglePatchReconstructionParameter, freqs)
+  return getSF(reco.sf, freqs, reco.sparseTrafo, reco.solver; toKwargs([pre, reco])...)
+end
+
 take!(algo::SinglePatchReconstruction) = take!(algo.output)
 
 # Similar to the already propossed generic reconstruction, but that's just this example
@@ -74,12 +88,48 @@ take!(algo::SinglePatchReconstruction) = take!(algo.output)
 # However with this more "free-form" put! method, we might have more code repetition
 # The idea here is to provide a lot of flexible and reusable "helper" functions with dispatch on parameters
 function put!(algo::SinglePatchReconstruction, data)
-  pre = process(algo, f, algo.params.pre)
-  result = reconstruct(algo, pre, algo.params.reco)
+  consistenceCheck(bSF, bMeas)
+  pre = process(algo, data, algo.params.pre)
+  result = process(algo, pre, algo.params.reco)
   if !isnothing(algo.params.post)
     result = process(algo, result, algo.params.post)
   end
   put!(algo.output, result)
+end
+
+function similar(algo::SinglePatchReconstruction, data::MPIFile)
+  pre = similar(algo, data, algo.param.pre)
+  reco = similar(algo, data, algo.param.reco)
+  post = similar(algo, data, algo.parms.post)
+
+  # This isnt a nice structure atm, because the corrections of the params cant be done individually
+  # and in certain parts should only be applied after construction
+
+  # Check if freq still valid
+  freqs = algo.freqs
+  S = algo.S
+  grid = algo.grid
+  if rxNumSamplingPoints(reco.sf) == rxNumSamplingPoints(data)
+    # Ensure that no frequencies are used that are not present in the measurement
+    freqs = intersect(algo.freqs, filterFrequencies(data, toKwargs(pre)))
+    if freqs != algo.freqs
+      S, grid = getSF(params.pre, params.reco, freqs)
+    end
+  end
+
+  result = SinglePatchReconstruction(SinglePatchReconstructionParameter(pre, reco, post), S, grid, freqs, Channel{Any}(32))
+
+  # TODO this only works for common preprocessing, because i directly access fields
+  # If S is processed and fits not to the measurements because of numPeriodsGrouping
+  # or numPeriodAverages being applied we need to set these so that the 
+  # measurements are loaded correctly
+  if rxNumSamplingPoints(bSF) > rxNumSamplingPoints(bMeas)
+    pre.numPeriodGrouping = rxNumSamplingPoints(bSF) ÷ rxNumSamplingPoints(bMeas)
+  end
+  if acqNumPeriodsPerFrame(bSF) < acqNumPeriodsPerFrame(bMeas)
+    pre.numPeriodAverages = acqNumPeriodsPerFrame(bMeas) ÷ (acqNumPeriodsPerFrame(bSF) * numPeriodGrouping)
+  end
+  return result
 end
 
 # These dont need to be specified for each algorithm, but each algorithm can have unique behaviour
