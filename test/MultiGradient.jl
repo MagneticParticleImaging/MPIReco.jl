d588fd6ae11b7cd2af82db35cd26bec0d045f44b
nHackel <nHackel@users.noreply.github.com>
2023-07-20 07:07:27 +0000

    Update MultiGradientTests

--- test/MultiGradient.jl
+++ test/MultiGradient.jl
@@ -8,27 +8,30 @@ using MPIReco
   bSFs = MultiMPIFile([joinpath(datadir, "calibrations", "13.mdf"), joinpath(datadir, "calibrations", "14.mdf")])
   names = names = (:color, :x, :y, :z, :time)
 
-  c1 = reconstruction(bSFs, b;
-			    SNRThresh=2, frames=1, lambd=0.003, minFreq=80e3,
-			    recChannels=1:2,iterations=3, roundPatches=false)
+  plan = getPlan("MultiPatch")
+  setAll!(plan, :SNRThresh, 2)
+  setAll!(plan, :frames, [1])
+  setAll!(plan, :minFreq, 80e3)
+  setAll!(plan, :recChannels, 1:2)
+  setAll!(plan, :iterations, 3)
+  setAll!(plan, :sf, bSFs)
+  setAll!(plan, :λ, 0.003)
+  setAll!(plan, :tfCorrection, false)
+  setAll!(plan, :roundPatches, false)
+
+  c1 = reconstruct(build(plan), b)
   @test axisnames(c1) == names
   @test axisvalues(c1) == (1:1, -26.0u"mm":1.0u"mm":26.0u"mm", -26.75u"mm":1.0u"mm":26.25u"mm", 0.0u"mm":1.0u"mm":0.0u"mm", 0.0u"ms":0.6528u"ms":0.0u"ms")
   im1 = reverse(c1[1,:,:,1,1]',dims=1)
   exportImage(joinpath(imgdir, "MultiGradient1.png"), im1)
   @test compareImg("MultiGradient1.png")
 
-  #Iabs = abs.(im1)
-  #Iabs[Iabs .< 0.2*maximum(Iabs)] .= 0
-  #Icolored = colorview(Gray, Iabs./maximum(Iabs))
-  #FileIO.save("$imgdir/MultiGradient1.png", Icolored )
 
   # low gradient with all 3 high gradient patches
   dirs = ["1.mdf", "2.mdf", "3.mdf", "4.mdf"]
   b = MultiMPIFile(joinpath.(datadir, "measurements", "20211226_203921_MultiGradient",  dirs))
 
-  c2 = reconstruction(bSFs, b;
-			    SNRThresh=2, frames=1, lambd=0.003, minFreq=80e3,
-			    recChannels=1:2,iterations=3, roundPatches=false)
+  c2 = reconstruct(build(plan), b)
   @test axisnames(c2) == names
   # TODO wo kommt dieser Floatingpoint Fehler rein? Lässt der sich vermeiden?
   @test axisvalues(c2) == (1:1, -26.0u"mm":1.0u"mm":26.0u"mm", -26.500000000000004u"mm":1.0u"mm":30.499999999999996u"mm", 0.0u"mm":1.0u"mm":0.0u"mm", 0.0u"ms":0.6528u"ms":0.0u"ms")
@@ -36,25 +39,16 @@ using MPIReco
   exportImage(joinpath(imgdir, "MultiGradient2.png"), im2)
   @test compareImg("MultiGradient2.png")
 
-  #Iabs = abs.(im2)
-  #Iabs[Iabs .< 0.2*maximum(Iabs)] .= 0
-  #Icolored = colorview(Gray, Iabs./maximum(Iabs))
-  #FileIO.save("$imgdir/MultiGradient2.png", Icolored )
 
 
   # low gradient only
   b = MultiMPIFile([joinpath(datadir, "measurements", "20211226_203921_MultiGradient", "1.mdf")])
   bSFs = MultiMPIFile([joinpath(datadir, "calibrations", "13.mdf")])
 
-  c3 = reconstruction(bSFs, b;
-			    SNRThresh=2, frames=1, lambd=0.003, minFreq=80e3,
-			    recChannels=1:2,iterations=3, roundPatches=false)
+  setAll!(plan, :sf, bSFs)
+  c3 = reconstruct(build(plan), b)
   im3 = reverse(c3[1,:,:,1,1]',dims=1)
   exportImage(joinpath(imgdir, "MultiGradient3.png"), im3)
   @test compareImg("MultiGradient3.png")
 
-  #Iabs = abs.(im3)
-  #Iabs[Iabs .< 0.2*maximum(Iabs)] .= 0
-  #Icolored = colorview(Gray, Iabs./maximum(Iabs))
-  #FileIO.save("$imgdir/MultiGradient3.png", Icolored )
 end
