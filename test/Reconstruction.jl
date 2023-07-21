f477f9be5499235a243cb5758f68a77e126d2e2b
nHackel <nHackel@users.noreply.github.com>
2023-07-21 09:45:17 +0000

    Add multi color and interpolation single patch tests

--- test/Reconstruction.jl
+++ test/Reconstruction.jl
@@ -30,62 +30,51 @@ using MPIReco
   @test compareImg("Reconstruction1.png")
 
   # fused lasso
-  setAll!(plan, :iterations, 100)
-  setAll!(plan, :loadasreal, true)
-  setAll!(plan, :solver, FusedLasso)
-  setAll!(plan, :reg, [TVRegularization(0.01f0), L1Regularization(0.01f0)])
-  setAll!(plan, :normalizeReg, NoNormalization())
-  c2 = reconstruct(build(plan), b)
-  setAll!(plan, :iterations, 1)
-  setAll!(plan, :loadasreal, false)
-  setAll!(plan, :solver, Kaczmarz)
-  setAll!(plan, :reg, [L2Regularization(0.0f0)])
-  setAll!(plan, :normalizeReg, SystemMatrixBasedNormalization())
-  @test axisnames(c2) == names
-  @test axisvalues(c2) == values
-  exportImage(joinpath(imgdir, "Reconstruction2.png"), arraydata(c2[1,:,:,1,1]))
-  @test compareImg("Reconstruction2.png")
-#=
+  #setAll!(plan, :iterations, 100)
+  #setAll!(plan, :loadasreal, true)
+  #setAll!(plan, :solver, FusedLasso)
+  #setAll!(plan, :reg, [TVRegularization(0.01f0), L1Regularization(0.01f0)])
+  #setAll!(plan, :normalizeReg, NoNormalization())
+  #c2 = reconstruct(build(plan), b)
+  #setAll!(plan, :iterations, 1)
+  #setAll!(plan, :loadasreal, false)
+  #setAll!(plan, :solver, Kaczmarz)
+  #setAll!(plan, :reg, [L2Regularization(0.0f0)])
+  #setAll!(plan, :normalizeReg, SystemMatrixBasedNormalization())
+  #@test axisnames(c2) == names
+  #@test axisvalues(c2) == values
+  #exportImage(joinpath(imgdir, "Reconstruction2.png"), arraydata(c2[1,:,:,1,1]))
+  #@test compareImg("Reconstruction2.png")
+
   # with interpolation
-  c3 = reconstruction(bSF, b; SNRThresh=5, frames=1, minFreq=80e3,
-		      recChannels=1:2, gridsize=[100,100,1],iterations=1)
+  setAll!(plan, :gridding, SystemMatrixGriddingParameter(;gridsize=[100,100,1], fov = calibFov(bSF)))
+  c3 = reconstruct(build(plan), b)
   @test axisnames(c3) == names
   @test axisvalues(c3) == (values[1], -27.8u"mm":0.4u"mm":11.8u"mm", -11.8u"mm":0.4u"mm":27.8u"mm", values[4:5]...)
   exportImage(joinpath(imgdir, "Reconstruction3.png"), arraydata(c3[1,:,:,1,1]))
   @test compareImg("Reconstruction3.png")
 
   # with fov adpation and center shift
-  c4 = reconstruction(bSF, b; SNRThresh=5, frames=1, minFreq=80e3,
-		      recChannels=1:2, gridsize=[50,50,1],fov=[0.04,0.04,1],
-		      center=[0.0,-0.01,0], iterations=1)
+  setAll!(plan, :gridding, SystemMatrixGriddingParameter(;gridsize=[50,50,1], fov = [0.04,0.04,1], center = [0.0,-0.01,0]))
+  c4 = reconstruct(build(plan), b)
   @test axisnames(c4) == names
   # TODO Tobi: does this make sense?
   @test axisvalues(c4) == (values[1], -27.6u"mm":0.8u"mm":11.6u"mm", -11.6u"mm":0.8u"mm":27.6u"mm", 499.5u"mm":1000.0u"mm":499.5u"mm", values[5])
   exportImage(joinpath(imgdir, "Reconstruction4.png"), arraydata(c4[1,:,:,1,1]))
   @test compareImg("Reconstruction4.png")
 
-  # multi colored reconstruction (deprecated interface)
-  c5 = reconstruction([bSF,bSF], b;
-		      SNRThresh=5, frames=1, minFreq=80e3,
-		      recChannels=1:2, iterations=1)
+  setAll!(plan, :gridding, SystemMatrixGriddingParameter(;gridsize=calibSize(bSF), fov = calibFov(bSF)))
+
+  # multi colored reconstruction (new interface)
+  setAll!(plan, :sf, MultiContrastFile([bSF,bSF]))
+  c5 = reconstruct(build(plan), b)
   @test axisnames(c5) == names
   @test axisvalues(c5) == (1:2,values[2:end]...)
   exportImage(joinpath(imgdir, "Reconstruction5a.png"), arraydata(c5[1,:,:,1,1]))
   @test compareImg("Reconstruction5a.png")
   exportImage(joinpath(imgdir, "Reconstruction5b.png"), arraydata(c5[2,:,:,1,1]))
   @test compareImg("Reconstruction5b.png")
-
-  # multi colored reconstruction (new interface)
-  c5 = reconstruction(MultiContrastFile([bSF,bSF]), b;
-          SNRThresh=5, frames=1, minFreq=80e3,
-          recChannels=1:2, iterations=1)
-  @test axisnames(c5) == names
-  @test axisvalues(c5) == (1:2,values[2:end]...)
-  exportImage(joinpath(imgdir, "Reconstruction5c.png"), arraydata(c5[1,:,:,1,1]))
-  @test compareImg("Reconstruction5c.png")
-  exportImage(joinpath(imgdir, "Reconstruction5d.png"), arraydata(c5[2,:,:,1,1]))
-  @test compareImg("Reconstruction5d.png")
-
+#=
   # dict based reco
   r = defaultRecoParams()
   r[:measPath] = filepath(b)
@@ -100,7 +89,8 @@ using MPIReco
   @test axisvalues(c6) == values
   exportImage(joinpath(imgdir, "Reconstruction6.png"), arraydata(c6[1,:,:,1,1]))
   @test compareImg("Reconstruction6.png")
-
+  
+  # TODO implement weighting
   # channel weighting
   c7a = reconstruction(bSF, b; SNRThresh=5, frames=1, minFreq=80e3,
 		      recChannels=1:2, iterations=1, λ=0.1,
