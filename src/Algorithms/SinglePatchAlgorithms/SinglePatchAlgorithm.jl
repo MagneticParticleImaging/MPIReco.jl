b2889bc3edf8907fc0e84a3c2469c4f42903ddbe
nHackel <nHackel@users.noreply.github.com>
2024-07-18 13:35:23 +0200

    Allow single patch weighting to be cached

--- src/Algorithms/SinglePatchAlgorithms/SinglePatchAlgorithm.jl
+++ src/Algorithms/SinglePatchAlgorithms/SinglePatchAlgorithm.jl
@@ -6,7 +6,7 @@ Base.@kwdef struct SinglePatchReconstructionParameter{L<:AbstractSystemMatrixLoa
   # Solver
   solverParams::SP
   reg::Vector{R} = AbstractRegularization[]
-  weightingParams::W = NoWeightingParameters()
+  weightingParams::Union{W, ProcessResultCache{W}} = NoWeightingParameters()
 end
 
 Base.@kwdef mutable struct SinglePatchReconstructionAlgorithm{P} <: AbstractSinglePatchReconstructionAlgorithm where {P<:AbstractSinglePatchAlgorithmParameters}
@@ -61,7 +61,7 @@ function process(algo::SinglePatchReconstructionAlgorithm, params::SinglePatchRe
   return gridresult(result, algo.grid, algo.sf)
 end
 
-process(algo::SinglePatchReconstructionAlgorithm, params::Union{ChannelWeightingParameters, WhiteningWeightingParameters}, u) = map(real(eltype(algo.S)), process(typeof(algo), params, algo.freqs))
+process(algo::SinglePatchReconstructionAlgorithm, params::Union{W, ProcessResultCache{W}}, u) where {W <: Union{ChannelWeightingParameters, WhiteningWeightingParameters}} = map(real(eltype(algo.S)), process(typeof(algo), params, algo.freqs))
 
 function getLinearOperator(algo::SinglePatchReconstructionAlgorithm, params::SinglePatchReconstructionParameter{<:DenseSystemMatixLoadingParameter, S}) where {S}
   return nothing
