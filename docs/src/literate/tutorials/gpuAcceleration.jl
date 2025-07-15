0def168e246db2f3d9c4d62bdb8a662459d1937d
nHackel <nHackel@users.noreply.github.com>
2025-07-15 18:46:21 +0200

    Hide more output from docs

--- docs/src/literate/tutorials/gpuAcceleration.jl
+++ docs/src/literate/tutorials/gpuAcceleration.jl
@@ -6,7 +6,7 @@ include("../../download.jl") #hide
 # using CUDA
 # gpu = CuArray
 # ```
-gpu = Array #hide # comment out when using proper GPUs
+gpu = Array; #hide # comment out when using proper GPUs
 
 # Afterwards one can use the normal reconstruction interface and specify the `arrayType` parameter.
 # However, the default solver is Kaczmarz, which has poor GPU performance. One should instead use CGNR or other solver.
@@ -25,7 +25,7 @@ params[:weightingParams] = WhiteningWeightingParameters(whiteningMeas = bSF)
 params[:reg] = [L2Regularization(0.1f0)]
 
 # Specifying the GPU array type to use, allows reconstruction algorithm to move the system matrix and data to the GPU after frequency filtering:
-c = reconstruct("SinglePatch", b; params..., arrayType = gpu, iterations = 100, solver = CGNR)
+c = reconstruct("SinglePatch", b; params..., arrayType = gpu, iterations = 100, solver = CGNR);
 
 # The resulting image is moved to the CPU at the end of the reconstruction and is accessible like any other reconstruction:
 using CairoMakie
