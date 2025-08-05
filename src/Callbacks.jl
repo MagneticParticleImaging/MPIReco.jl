07aa4feab85dd7b7b87af2bec5cd35f399c22f3c
nHackel <nHackel@users.noreply.github.com>
2025-08-05 17:34:24 +0200

    Add StoreSolutionPerCallback

--- src/Callbacks.jl
+++ src/Callbacks.jl
@@ -1,4 +1,4 @@
-export CompareSolutionPerFrameCallback
+export CompareSolutionPerFrameCallback, StoreSolutionPerFrameCallback
 mutable struct CompareSolutionPerFrameCallback{T, F}
   ref::Matrix{T}
   cmp::F
@@ -13,4 +13,18 @@ function (cb::CompareSolutionPerFrameCallback)(solver, frame, _)
   end
   c = solversolution(solver)
   push!(cb.results[frame], cb.cmp(cb.ref[:, frame], c))
+end
+
+mutable struct StoreSolutionPerFrameCallback{T}
+  solutions::Vector{Vector{Vector{T}}}
+  frame::Int64
+  StoreSolutionPerFrameCallback(T::Type = Float32) = new{T}(Vector{Vector{Vector{T}}}(), 0)
+end
+function (cb::StoreSolutionPerFrameCallback{T})(solver, frame, _) where T
+  if frame != cb.frame
+    push!(cb.solutions, Vector{Vector{T}}())
+    cb.frame = frame
+  end
+  c = deepcopy(solversolution(solver))
+  push!(cb.solutions[frame], c)
 end
